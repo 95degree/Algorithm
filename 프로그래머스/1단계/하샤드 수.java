cd5c98fd4523bbe9d9efa9b0fa03a317601ae457
Marco <73640185+95degree@users.noreply.github.com>
2021-06-05 20:03:38 +0900

    Update 하샤드 수.java

--- 프로그래머스/1단계/하샤드 수.java
+++ 프로그래머스/1단계/하샤드 수.java
@@ -1,4 +1,4 @@
-import java.util. *;
+import java.util.*;
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
@@ -6,6 +6,22 @@ class Solution {
         int i =0;
         for(String num : stringNumberArray){
             i+=Integer.parseInt(num);
+        }
+        answer = (x%i)==0;
+        return answer;
+    }
+}
+
+//char로 풀어본 결과 시간이 훨씬 빠르다 메모리는 비슷
+import java.util. *;
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        char[] charNumberArray = Integer.toString(x).toCharArray();
+        int i =0;
+        for(char charNum : charNumberArray){
+            
+            i+= charNum-'0';
         }
         answer = (x%i)==0;
         return answer;
