ac0440d880d735f3a7cf753bcc9bf7741fb61bb8
Marco <73640185+95degree@users.noreply.github.com>
2021-06-08 20:32:17 +0900

    Update 정수 제곱근 판별.java

--- 프로그래머스/1단계/정수 제곱근 판별.java
+++ 프로그래머스/1단계/정수 제곱근 판별.java
@@ -3,7 +3,7 @@ class Solution {
     public long solution(long n) {
         long answer = -1;
         double num = Math.sqrt(n);
-        if(num %1 == 0) {
+        if(num%1 == 0) {
             answer = (long)Math.pow(num+1,2);
         }
         return answer;
