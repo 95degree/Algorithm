64b8d0fb77a6c8b339bca905cd11dd13364a694e
Marco <73640185+95degree@users.noreply.github.com>
2021-06-08 20:30:46 +0900

    Create 정수 제곱근 판별.java

---
+++ 프로그래머스/1단계/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        double num = Math.sqrt(n);
+        if(num %1 == 0) {
+            answer = (long)Math.pow(num+1,2);
+        }
+        return answer;
+    }
+}
