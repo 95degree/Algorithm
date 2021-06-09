bab30946b18b274e1438ec6ce39df629a91db334
Marco <73640185+95degree@users.noreply.github.com>
2021-06-09 19:18:08 +0900

    Create 정수 내림차순으로 배치하기.java

---
+++ 프로그래머스/1단계/정수 내림차순으로 배치하기.java
@@ -0,0 +1,18 @@
+import java.util. *;
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        char[] numCharArray = String.valueOf(n).toCharArray();
+        List<Integer> list = new ArrayList<>();
+        for(char num : numCharArray){
+            list.add(num -'0');
+        }
+        Collections.sort(list,Collections.reverseOrder());
+        StringBuilder sr = new StringBuilder();
+        for(Integer num : list){
+            sr.append(num.toString());
+        }
+        answer = Long.parseLong(sr.toString());
+        return answer;
+    }
+}
