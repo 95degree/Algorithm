322dd79b2201127a5787c9573213d96f0f5c8949
Marco <73640185+95degree@users.noreply.github.com>
2021-06-12 20:21:51 +0900

    Update 약수의 합.java

--- 프로그래머스/1단계/약수의 합.java
+++ 프로그래머스/1단계/약수의 합.java
@@ -9,3 +9,23 @@ class Solution {
         return answer;
     }
 }
+
+// O(sqrt(n)) 방법 주어진 수의 제곱근 보다 작은 정수의 약수를 구하고 주어진 수로 나눠서 나머지 약수를 구해 더한다.
+
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        double num = Math.sqrt(n);
+        for(int i=1; i<=(int)num; i++){
+            if(n%i ==0){
+                int a = n/i;
+                answer+=i;
+                if(a != i){
+                    answer += a;
+                }
+            }
+        }
+        return answer;
+    }
+}
