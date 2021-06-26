db732f0272daa87a3b8b2a9ecf40e9c4f7cde3bc
Marco <73640185+95degree@users.noreply.github.com>
2021-06-26 18:30:05 +0900

    Update 크레인 인형뽑기.java

--- 프로그래머스/1단계/크레인 인형뽑기.java
+++ 프로그래머스/1단계/크레인 인형뽑기.java
@@ -5,9 +5,9 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<moves.length; i++){
             for(int j=0; j<board.length; j++){
-                if(board[j][moves[i]-1] != -1 && board[j][moves[i]-1] !=0){
+                if(board[j][moves[i]-1] !=0){
                     list.add(board[j][moves[i]-1]);
-                    board[j][moves[i]-1] = -1;
+                    board[j][moves[i]-1] = 0;
                     if(list.size()-1 > 0 && list.get(list.size()-1) == list.get(list.size()-2)){
                         list.remove(list.size()-1);
                         list.remove(list.size()-1);
