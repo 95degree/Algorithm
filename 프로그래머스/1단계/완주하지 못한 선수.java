534f293fdb8fe4040755fbecaf9c9e82c457c0a0
Marco <73640185+95degree@users.noreply.github.com>
2021-07-01 20:40:39 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/1단계/완주하지 못한 선수.java
+++ 프로그래머스/1단계/완주하지 못한 선수.java
@@ -17,3 +17,19 @@ class Solution {
         return answer;
     }
 }
+
+//정렬로 풀어보기 시간은 훨씬 오래 걸림....
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer ="";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for(int i =0; i<completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[participant.length-1];
+    }
+}
