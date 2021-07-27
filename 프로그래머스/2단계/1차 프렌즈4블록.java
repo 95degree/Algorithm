56fac4ba5d7fda619ab6f7dff9e223d716d3e22f
Marco <73640185+95degree@users.noreply.github.com>
2021-07-27 22:51:31 +0900

    Create 1차 프렌즈4블록.java

---
+++ 프로그래머스/2단계/1차 프렌즈4블록.java
@@ -0,0 +1,71 @@
+//실패 내일 다시 도전//
+import java.util.*;
+class Solution {
+    public boolean[][] map;
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        char[][] array = new char[m][n];
+        map = new boolean[m][n];
+        for(int i =0; i<board.length; i++){
+           array[i] = board[i].toCharArray(); 
+        }
+        while(deleteBlock(array)){
+            swap(array);
+        }
+    
+        for(int i =0; i<map.length; i++){
+            for(int j =0; j<map[i].length; j++){
+                if(map[i][j]){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    public boolean deleteBlock(char[][] array){
+        boolean bool = false;
+         for(int i =1; i<array.length; i++){
+            for(int j =1; j<array[i].length; j++){
+                char up = array[i-1][j];
+                char left = array[i][j-1];
+                char sideUp = array[i-1][j-1];
+                char peek = array[i][j];
+                if(peek != ' ' && up == peek && left == peek && sideUp == peek){
+                    map[i-1][j] = true;
+                    map[i][j-1] = true;
+                    map[i-1][j-1] = true;
+                    map[i][j] = true;
+                    bool = true;
+                }
+            }
+        }
+        return bool;
+    }
+        
+        public void swap(char[][] array){
+            for(int i =0 ; i<map.length; i++){
+                for(int j = 0; j<map[i].length; j++){
+                    if(map[i][j]){
+                        array[i][j] = ' ';
+                    }
+                }
+            }
+            for(int i =array.length-1; i>=0; i--){
+                for(int j = 0; j<array[i].length; j++){
+                    if(array[i][j] == ' '){
+                        int index = i;
+                        while(index>0){
+                            index--;
+                            if(array[index][j] == ' '){
+                                array[i][j] = array[index][j];
+                                array[index][j] = ' ';
+                                map[i][j] = false;
+                                map[index][j] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }        
+}
