983217b35f794451ffd3f03e3db7bb5f529d3d23
Marco <73640185+95degree@users.noreply.github.com>
2021-08-06 22:56:51 +0900

    Update 프린터 큐_1966번.java

--- 백준/프린터 큐_1966번.java
+++ 백준/프린터 큐_1966번.java
@@ -1,3 +1,57 @@
+//이너클래스로 구현//
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static class Node {
+        public int index;
+        public int number;
+
+        public Node(int number, int index) {
+            this.index = index;
+            this.number = number;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sr = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int answer =0;
+            Queue<Node> queue = new LinkedList<>();
+            List<Integer> list = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int length = Integer.parseInt(st.nextToken());
+            int check = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < length; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                queue.add(new Node(num, j));
+                list.add(num);
+            }
+            list.sort(Collections.reverseOrder());
+            while (true) {
+                Node print = queue.poll();
+                if (list.get(0) == print.number){
+                    if(print.index == check){
+                        answer++;
+                        break;
+                    }
+                    list.remove(0);
+                    answer++;
+                }
+                else{
+                    queue.add(print);
+                }
+            }
+            sr.append(answer).append("\n");
+        }
+        System.out.print(sr);
+    }
+}
+
+//이너클래스 없이 구현//
 import java.util.*;
 import java.io.*;
 
