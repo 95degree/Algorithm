987e1f86071c7db16564427cfc8fe5246a31c300
Marco <73640185+95degree@users.noreply.github.com>
2021-08-08 21:41:09 +0900

    Update 분해합_2231번.java

--- 백준/분해합_2231번.java
+++ 백준/분해합_2231번.java
@@ -24,3 +24,28 @@ public class Main{
         System.out.println(check);
     }
 }
+//문자열로 풀기//
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer>list = new ArrayList<>();
+        for (int i = n; i>0; i--) {
+            String num = String.valueOf(i);
+            int answer = 0;
+            for(int j =0; j<num.length(); j++){
+                answer+=num.charAt(j) - '0';
+            }
+            if(answer+i == n){
+                list.add(i);
+            }
+        }
+        int check = 0;
+        if(list.size()>0){
+            Collections.sort(list);
+            check = list.get(0);
+        }
+        System.out.println(check);
+    }
+}
