2c99be461a2c6335d45889fd80c761a22f12fcd3
Marco <95degree@naver.com>
2021-08-12 20:54:26 +0900

    Create 설탕배달_2839번.java

---
+++ 백준/설탕배달_2839번.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            int answer = 0;
+            while(n>0){
+                if(n%5 ==0){
+                    answer += n/5;
+                    n/=5;
+                    break;
+                }
+                n -=3;
+                answer++;
+            }
+            if(n<0){
+                answer = -1;
+            }
+            System.out.println(answer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
