f8605187bb7d20446b7fcf46c2983cae21054b7e
Marco <95degree@naver.com>
2021-08-24 22:42:23 +0900

    Update 유기농 배추_1012번.java

--- 백준/유기농 배추_1012번.java
+++ 백준/유기농 배추_1012번.java
@@ -1,3 +1,4 @@
+//DFS//
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -52,6 +53,79 @@ public class Main {
                     }
                     ground[checkX][checkY] = 0;
                     dfs(checkX,checkY);
+            }
+        }
+    }
+}
+
+//BFS//
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int[] dr = {-1,1,0,0};
+    public static int[] dc = {0,0,-1,1};
+    public static int[][] ground;
+    public static int m;
+    public static int n;
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int t = Integer.parseInt(br.readLine());
+            StringBuilder sr = new StringBuilder();
+            for(int i =0; i<t; i++){
+                int answer = 0;
+                StringTokenizer str = new StringTokenizer(br.readLine());
+                m = Integer.parseInt(str.nextToken());
+                n = Integer.parseInt(str.nextToken());
+                ground = new int[m][n];
+                int k = Integer.parseInt(str.nextToken());
+                for(int j =0; j<k; j++){
+                    str = new StringTokenizer(br.readLine());
+                    int x = Integer.parseInt(str.nextToken());
+                    int y = Integer.parseInt(str.nextToken());
+                    ground[x][y] = 1;
+                }
+                for(int j =0; j<m; j++){
+                    for(int p =0; p<n; p++){
+                        if(ground[j][p] == 1){
+                            bfs(j,p);
+                            answer++;
+                        }
+                    }
+                }
+                sr.append(answer).append("\n");
+            }
+            System.out.println(sr);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void bfs(int r, int c){
+        int[] position = new int[2];
+        Queue<int[]>queue = new LinkedList<>();
+        position[0] = r;
+        position[1] = c;
+        queue.add(position);
+        while(!queue.isEmpty()){
+            int[] xy = queue.poll();
+            ground[xy[0]][xy[1]] = 0;
+            for(int q =0; q<4; q++){
+                int nr = xy[0]+dr[q];
+                int nc = xy[1]+dc[q];
+                if(nr>=0 && nc>=0 && nr<m && nc<n){
+                    if(ground[nr][nc]==1){
+                        int[] tmp = new int[2];
+                        tmp[0] = nr;
+                        tmp[1] = nc;
+                        queue.add(tmp);
+                        ground[tmp[0]][tmp[1]] = 0;
+                    }
+                }
             }
         }
     }
