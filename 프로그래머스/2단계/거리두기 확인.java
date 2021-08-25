a472bf7f87672290d52239b75f69241271d00fa8
Marco <95degree@naver.com>
2021-08-25 11:30:20 +0900

    Update 거리두기 확인.java

--- 프로그래머스/2단계/거리두기 확인.java
+++ 프로그래머스/2단계/거리두기 확인.java
@@ -47,3 +47,64 @@ class Solution {
         return 1;
     }
 }
+
+//DFS로 풀기//
+class Solution {
+    public int[] checkX = {1,-1,0,0};
+    public int[] checkY = {0,0,1,-1};
+
+    public int[] solution(String[][] places) {
+        int[] answer = new int [5];
+        for(int i =0; i<places.length; i++){
+            answer[i]=check(room(places[i]));
+        }
+        return answer;
+    }
+
+    private char[][] room(String[] place){
+        char[][] room = new char[5][5];
+        for(int i =0; i<place.length; i++){
+            String str = place[i];
+            for(int j =0; j<str.length(); j++){
+                room[i][j] = str.charAt(j);
+            }
+        }
+        return room;
+    }
+
+    private int check(char[][] room){
+        for(int i =0; i<room.length; i++){
+            for(int j =0; j<room[i].length; j++){
+                if(room[i][j] == 'P'){
+                   if(dfs(i,j,room,0)==0){
+                       return 0;
+                   }
+                }
+            }
+        }
+        return 1;
+    }
+
+    private int dfs(int y, int x, char[][]room, int depth){
+        room[y][x] = 'X';
+        for(int i =0; i<checkX.length; i++){
+            int y1 = y+checkY[i];
+            int x1 = x+checkX[i];
+            if(y1>=0 && x1>=0 && y1<5 && x1<5){
+                if(room[y1][x1] == 'O' && depth == 0){
+                    room[y1][x1] = 'X';
+                    if(dfs(y1,x1,room,depth+1) == 0){
+                        return 0;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+                if(room[y1][x1] == 'P'){
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+}
