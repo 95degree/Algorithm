792bac7eb8d5c56457b04e2824508b88d8af311e
Marco <95degree@naver.com>
2021-09-05 22:30:34 +0900

    Update 회의실 배정_1931번.java

--- 백준/회의실 배정_1931번.java
+++ 백준/회의실 배정_1931번.java
@@ -1,3 +1,4 @@
+//이너클래스로 풀기//
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -37,6 +38,49 @@ public class Main {
                 if (start >= lastEnd) {
                     answer++;
                     lastEnd = meetings[i].end;
+                }
+                i++;
+            }
+            System.out.println(answer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//2차원배열로 풀기//
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] meetings = new int[n][2];
+            int answer = 0;
+            for (int i = 0; i < n; i++) {
+                StringTokenizer str = new StringTokenizer(br.readLine());
+                for(int j =0; j<2; j++){
+                    meetings[i][j] = Integer.parseInt(str.nextToken());
+                }
+            }
+            Arrays.sort(meetings, (o1, o2) -> {
+                if(o1[1]==o2[1]) {
+                    return o1[0]-o2[0];
+                }
+                return o1[1]-o2[1];
+            });
+            int i =0;
+            int lastEnd = 0;
+            while(i<n){
+                int start = meetings[i][0];
+                if(start>=lastEnd){
+                    answer++;
+                    lastEnd = meetings[i][1];
                 }
                 i++;
             }
