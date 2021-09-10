14e98c1be4800e6d8b1c85c78d49d2f8239ac343
Marco <95degree@naver.com>
2021-09-10 17:22:02 +0900

    Update 실패율.java

--- 프로그래머스/1단계/실패율.java
+++ 프로그래머스/1단계/실패율.java
@@ -1,3 +1,53 @@
+//정렬 구현 좀더 빠르다//
+import java.util.*;
+class Solution {
+    public class Fail{
+        int step;
+        double failure;
+        
+        public Fail(int step, double failure){
+            this.step = step;
+            this.failure = failure;
+        }
+    }
+    
+    public int[] solution(int N, int[] stages) {
+         Fail[] failes = new Fail[N];
+         int[] answer = new int[N+1];
+         double[] people = new double[N+1];
+         int[] rank = new int[N];
+         for(int num : stages){
+             answer[num-1]++;
+             for(int i =0; i<num; i++){
+                 people[i]++;
+             }
+         }
+        
+         for(int i =0; i<failes.length; i++){
+             double fail = 0;
+             if(people[i] !=0){
+                 fail = answer[i]/people[i];
+             }
+             failes[i] = new Fail(i+1, fail);
+         }
+         
+        Arrays.sort(failes,(o1,o2) ->{
+           int compare = Double.compare(o2.failure,o1.failure);
+           if(compare == 0){
+               return o1.step-o2.step;
+           }
+           return compare;
+        });
+        
+         for(int i =0; i<failes.length; i++){
+             rank[i] = failes[i].step;
+         }
+        
+         return rank;
+    }
+}
+
+//save List 만들어서 비교//
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
