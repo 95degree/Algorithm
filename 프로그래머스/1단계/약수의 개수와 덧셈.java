d3107420a62ddf1bdd91ede4d77c95b2d2ed6de7
Marco <95degree@naver.com>
2021-09-27 14:38:09 +0900

    Update 약수의 개수와 덧셈.java

--- 프로그래머스/1단계/약수의 개수와 덧셈.java
+++ 프로그래머스/1단계/약수의 개수와 덧셈.java
@@ -3,29 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        for(int i=left; i<=right; i++){
-            int num = count(i);
-            if(num%2 ==0){
-                answer+=i;
-            }
-            else{
-                answer-=i;
+        for(int i = left; i<=right; i++){
+            int count = count(i);
+            answer += i;
+            if(count%2 == 1){
+                answer-=2*i;
             }
         }
         return answer;
     }
     
-    private int count(int num){
+    private int count (int num){
         int max = (int)Math.sqrt(num);
-        int count =0;
-        for(int i=1; i<=max; i++){
+        int answer = 0;
+        for(int i = 1; i<=max; i++){
             if(num%i == 0){
-                count+=2;
-                if(i*i == num)
-                    count-=1;
+                answer+=2;
+                if(i*i == num){
+                    answer--;
+                }
             }
         }
-        return count;
+        return answer;
     }
 }
 
