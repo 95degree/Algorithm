2e1ecc295ebe157518d875173dceeb36b1ea4078
Marco <95degree@naver.com>
2021-11-25 22:28:08 +0900

    Update 다리를 지나는 트럭.java

--- 프로그래머스/2단계/다리를 지나는 트럭.java
+++ 프로그래머스/2단계/다리를 지나는 트럭.java
@@ -31,3 +31,36 @@ class Solution {
         return answer;
     }
 }
+
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int index = 0;
+        int sum = 0;
+        Queue<Integer>queue = new LinkedList<>();
+        for(int i =0; i<bridge_length; i++){
+            queue.add(0);
+        }
+        while(!queue.isEmpty()){
+          answer++;
+          if(queue.size() == bridge_length){
+               sum -= queue.poll();
+          }
+          if(index<truck_weights.length){
+              int truck = truck_weights[index];
+              if(sum+truck<=weight && queue.size()<bridge_length){
+                  queue.add(truck);
+                  index++;
+                  sum += truck;
+              } else {
+                  queue.add(0);
+              }
+              continue;
+          }
+          return answer + bridge_length-1;
+        }
+        return answer;
+    }
+}
