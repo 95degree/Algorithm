644c400f21a7f587593431b56de848ca95ace57a
Marco <95degree@naver.com>
2022-01-15 17:07:22 +0900

    Update 타겟넘버.java

--- 프로그래머스/2단계/타겟넘버.java
+++ 프로그래머스/2단계/타겟넘버.java
@@ -2,6 +2,37 @@ import java.util.*;
 class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
+        dfs(numbers,target,0);
+        return answer;
+    }
+    
+    public void dfs(int[]numbers, int target, int index){
+        if(index == numbers.length){
+            if(possible(numbers,target)){
+                answer++;
+            }
+            return;
+        }
+        for(int i =0; i<2; i++){
+           numbers[index]*=-1;
+           dfs(numbers,target,index+1); 
+        }
+    }
+    
+    public boolean possible(int[] numbers, int target){
+        int count = 0;
+        for(int num : numbers){
+            count += num;
+        }
+        return count == target;
+    }
+}
+
+
+import java.util.*;
+class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
         dfs(0,numbers,target,0);
         return answer;
     }
