22c570512f3f6d5e86871e8c2b48f61991420eb0
Marco <95degree@naver.com>
2022-02-16 11:24:19 +0900

    Update 나무 자르기_2805번.java

--- 백준/나무 자르기_2805번.java
+++ 백준/나무 자르기_2805번.java
@@ -4,6 +4,71 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
+    static StringBuilder sr = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] w;
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        output();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        w = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            w[i] = Integer.parseInt(st.nextToken());
+        }
+        twoSearch();
+    }
+
+
+    static void twoSearch() {
+        long left = 0;
+        long right = 1000000000;
+        long answer = 0;
+        while(left<=right){
+            int mid = (int)((left+right)/2);
+            if(cut(mid)){
+                answer = mid;
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    static boolean cut(int idx){
+        long answer = 0;
+        for(int num : w){
+            long c = num - idx;
+            if(c>0){
+                answer+=c;
+            }
+        }
+        return answer >=m;
+    }
+
+    static void output() {
+        System.out.println(sr);
+    }
+}
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             StringTokenizer str = new StringTokenizer(br.readLine());
