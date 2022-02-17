12fc816b02938ffee83fe3b945e61fda6e1b187f
Marco <95degree@naver.com>
2022-02-17 10:22:46 +0900

    Update 수 찾기_1920번.java

--- 백준/수 찾기_1920번.java
+++ 백준/수 찾기_1920번.java
@@ -1,3 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sr = new StringBuilder();
+    static int n;
+    static int[] w;
+    static int m;
+    static int[] p;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        output();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        w = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            w[i] = Integer.parseInt(st.nextToken());
+        }
+        m = Integer.parseInt(br.readLine());
+        p = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(w);
+        for (int num : p) {
+            sr.append(twoPointer(num)).append("\n");
+        }
+    }
+
+    static int twoPointer(int target) {
+        int left = 0;
+        int right = n-1;
+        int answer = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (w[mid] <= target) {
+                left = mid + 1;
+                answer = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return w[answer] == target ? 1 : 0;
+    }
+
+    static void output() {
+        System.out.println(sr);
+    }
+}
+
+
 import java.util.*;
 import java.io.*;
 public class Main{
