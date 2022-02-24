f8a895c7effa5e30477e26a72a1d26dcebd58b4c
Marco <95degree@naver.com>
2022-02-24 11:25:22 +0900

    Update 물통.java

--- 백준/물통.java
+++ 백준/물통.java
@@ -6,74 +6,79 @@ import java.util.*;
 public class Main {
 
     static StringBuilder sr = new StringBuilder();
-    static Water water;
-    static boolean check[][][] = new boolean[201][201][201];
+    static int n;
+    static int m;
+    static int v;
+    static boolean[][][] check;
+    static int[] volume;
     static Set<Integer> set = new TreeSet<>();
-    static int[] limit;
 
     static class Water {
-        int x[];
+
+        private int[] amount;
 
         public Water(int[] water) {
-            x = new int[]{water[0], water[1], water[2]};
+            amount = new int[]{water[0], water[1], water[2]};
         }
 
-        public Water move(int to, int from, int[] limit) {
-            int[] nX = new int[]{x[0], x[1], x[2]};
-            if (x[to] + x[from] <= limit[to]) {
-                nX[to] += nX[from];
-                nX[from] = 0;
+        public Water move(int to, int from) {
+            int[] newAmount = amount.clone();
+            if (amount[to] + amount[from] <= volume[to]) {
+                newAmount[to] += newAmount[from];
+                newAmount[from] = 0;
             } else {
-                nX[from] -= limit[to] - nX[to];
-                nX[to] = limit[to];
+                newAmount[from] -= volume[to] - newAmount[to];
+                newAmount[to] = volume[to];
             }
-            return new Water(nX);
+            return new Water(newAmount);
         }
 
-        public boolean possible() {
-            return x[0] == 0;
+        public boolean finish() {
+            return amount[0] == 0;
         }
     }
 
     public static void main(String[] args) throws IOException {
         input();
-        bfs(0, 0, limit[2]);
+        bfs(0, 0, v);
         output();
     }
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        limit = new int[]{sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+        volume = new int[]{n, m, v};
+        check = new boolean[201][201][201];
     }
 
-    static void output() {
-        for (int num : set) {
-            sr.append(num).append(" ");
-        }
-        System.out.println(sr.toString());
-    }
-
-    static void bfs(int x1, int x2, int x3) {
+    static void bfs(int x, int y, int z) {
         Queue<Water> queue = new LinkedList<>();
-        queue.add(new Water(new int[]{x1, x2, x3}));
-        check[x1][x2][x3] = true;
+        queue.add(new Water(new int[]{x, y, z}));
+        check[x][y][z] = true;
         while (!queue.isEmpty()) {
-            Water checkWater = queue.poll();
-            if (checkWater.possible()) {
-                set.add(checkWater.x[2]);
+            Water water = queue.poll();
+            if (water.finish()) {
+                set.add(water.amount[2]);
             }
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (i != j) {
-                        Water next = checkWater.move(i, j, limit);
-                        if (!check[next.x[0]][next.x[1]][next.x[2]]) {
-                            queue.add(next);
-                            check[next.x[0]][next.x[1]][next.x[2]] = true;
-                        }
-                    }
+                    if (i == j) continue;
+                    Water newWater = water.move(i, j);
+                    if (check[newWater.amount[0]][newWater.amount[1]][newWater.amount[2]]) continue;
+                    queue.add(newWater);
+                    check[newWater.amount[0]][newWater.amount[1]][newWater.amount[2]] = true;
                 }
             }
         }
+    }
+
+    static void output() {
+        for (Integer integer : set) {
+            sr.append(integer).append(" ");
+        }
+        System.out.println(sr);
     }
 }
