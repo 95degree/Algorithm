0a7bd77573ad2d10da1227ca1d7c3311597cedb0
Marco <95degree@naver.com>
2022-02-24 10:35:09 +0900

    Update DFS BFS_1260번.java

--- 백준/DFS BFS_1260번.java
+++ 백준/DFS BFS_1260번.java
@@ -1,3 +1,79 @@
+//인접 리스트//
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sr = new StringBuilder();
+    static int n;
+    static int m;
+    static int v;
+    static List<List<Integer>> list = new ArrayList<>();
+    static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        dfs(v);
+        sr.append("\n");
+        Arrays.fill(check,false);
+        bfs(v);
+        output();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+        check = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+            Collections.sort(list.get(a));
+            Collections.sort(list.get(b));
+        }
+    }
+
+    static void dfs(int start) {
+        check[start] = true;
+        sr.append(start).append(" ");
+        for (int num : list.get(start)) {
+            if (check[num]) continue;
+            dfs(num);
+        }
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        check[start] = true;
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            sr.append(num).append(" ");
+            for (int newNum : list.get(num)) {
+                if (check[newNum]) continue;
+                queue.add(newNum);
+                check[newNum] = true;
+            }
+        }
+    }
+
+    static void output() {
+        System.out.println(sr);
+    }
+}
+
+//인접 행렬//
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
